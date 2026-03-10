b9d2874a07d680fb1ed7cd49a7ae8297c2b40b55
JothamWong <jwys6747@gmail.com>
2026-03-10 15:05:28 +0000

    Add utils.h

---
+++ utils.h
@@ -0,0 +1,17 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <bits/types/struct_timeval.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+#define DEV_PATH "/dev/dax0.0"
+#define DEV_SIZE (16 * 1024 * 1024ul)
+
+static inline double get_time() {
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return (double) tv.tv_sec + (double)tv.tv_usec / 1000000.0;
+}
+
+#endif
